41b6e36ee4f22e59eafaf7e74cfaf71bc8a6929b
Geovani <geovanimendoza.h@gmail.com>
2020-07-11 14:32:53 -0400

    refactor: add route geenrator

---
+++ lib/utils/route_generator.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_w_firebase/ui/login/login.dart';
+
+class RouteGenerator {
+  static Route<dynamic> generateRoute(RouteSettings settings) {
+    final dynamic args = settings.arguments;
+
+    switch (settings.name) {
+      case '/login':
+        return PageRouteBuilder(
+          pageBuilder: (context, animation, secondaryAnimation) => LoginPage(),
+        );
+        break;
+      case '/main_menu':
+        return PageRouteBuilder(
+        );
+        break;
+    }
+  }
+}
